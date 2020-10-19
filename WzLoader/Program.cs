1fe0c61f89d8da4ba271b439d8d9bd8c81353c11
MikyWang <813853090@qq.com>
2020-10-19 19:12:26 +0800

    Ver1.1

---
+++ WzLoader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WzLoader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
