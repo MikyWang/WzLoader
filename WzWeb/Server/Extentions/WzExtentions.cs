58ba51940059e4d464859b9d48d7cc9535e1db2d
miky Wang <813853090@qq.com>
2020-10-21 17:30:25 +0800

    * Node.cs: * FileInfo.cs: * FileTable.razor: * WzExtentions.cs:
    * Wz.razor: 显示文件信息

---
+++ WzWeb/Server/Extentions/WzExtentions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using WzLib;
+using WzWeb.Shared;
+using System.Linq;
+
+namespace WzWeb.Server.Extentions
+{
+    public static class WzNodeExtention
+    {
+        public static Node ToNode(this Wz_Node wz_Node)
+        {
+
+            return new Node
+            {
+                ParentNode = wz_Node.ParentNode?.ToNode(),
+                Text = wz_Node.Text,
+                Value = wz_Node.Value?.ToString(),
+                FullPath = wz_Node.FullPath,
+                Type = wz_Node.GetNodeType()
+            };
+        }
+
+        public static NodeType GetNodeType(this Wz_Node wz_Node)
+        {
+            var value = wz_Node.Value;
+            if (value == null) return NodeType.Wz_Normal;
+            if (value is Wz_File) return NodeType.Wz_File;
+            if (value is Wz_Image) return NodeType.Wz_Image;
+            if (value is Wz_Png) return NodeType.Wz_Png;
+            if (value is Wz_Sound) return NodeType.Wz_Sound;
+            if (value is Wz_Uol) return NodeType.Wz_Uol;
+            if (value is Wz_Vector) return NodeType.Wz_Vector;
+            return NodeType.Wz_Normal;
+        }
+    }
+}
--- WzWeb/Shared/Node.cs
+++ WzWeb/Shared/Node.cs
@@ -8,6 +8,19 @@ namespace WzWeb.Shared
         public Node ParentNode { get; set; }
         public string Text { get; set; }
         public string Value { get; set; }
+        public NodeType Type { get; set; }
+        public string FullPath { get; set; }
+    }
+
+    public enum NodeType
+    {
+        Wz_File,
+        Wz_Image,
+        Wz_Sound,
+        Wz_Png,
+        Wz_Uol,
+        Wz_Vector,
+        Wz_Normal
     }
 
 }
