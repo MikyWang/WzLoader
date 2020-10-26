7475ede9c50a55448d3135bb5f626d9e5caf425a
miky Wang <813853090@qq.com>
2020-10-26 19:49:34 +0800

    添加png

---
+++ WzWeb/Shared/PngInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace WzWeb.Shared
+{
+    public class PngInfo
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int DataLength { get; set; }
+        public int Form { get; set; }
+        public string Base64Data { get; set; }
+    }
+}
