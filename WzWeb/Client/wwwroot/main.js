3ca5b61f4391eb6fdeee3fb599f17d836be219dc
miky Wang <813853090@qq.com>
2020-10-29 11:54:21 +0800

    调用JS

---
+++ WzWeb/Client/wwwroot/main.js
@@ -0,0 +1,9 @@
+﻿window.getBrowserConfig = function () {
+    var cfg = {
+        innerWidth: window.innerWidth,
+        innerHeight: window.innerHeight,
+        outerHeight: window.outerHeight,
+        outerWidth: window.outerWidth
+    }
+    return cfg;
+}
